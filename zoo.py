d7ea052218c54749640e1099a04470b578de731a
Stoyan <stoyaneft@gmail.com>
2014-11-03 20:46:53 +0200

    calc incomes

--- zoo.py
+++ zoo.py
@@ -1,11 +1,14 @@
 class Zoo:
-    def __init__(self, name, capacity, budget):
+
+    INCOME_PER_ANIMAL = 60
+
+    def __init__(self, capacity, budget):
         self.animals = []
         self.capacity = capacity
         self.budget = budget
 
-##accomodate
-##calculate incomes
-##calculate outcomes
-##is dead
-## produce animals
+    def accommodate(self, animal):
+        self.animals.append(animal)
+
+    def calculate_daily_income(self):
+        return len(self.animals) * Zoo.INCOME_PER_ANIMAL
--- zoo_test.py
+++ zoo_test.py
@@ -1,9 +1,23 @@
 import unittest
 
+from zoo import Zoo
+from animal import Animal
+
 
 class TestZoo(unittest.TestCase):
+
     def setUp(self):
-        pass
+        self.zoo = Zoo(10, 1000)
+        self.tiger = Animal('Tiger', 2, 'Gosho', 'Male', 20)
+        self.zoo.accommodate(self.tiger)
+
+    def test_accommodate(self):
+        self.assertIn(self.tiger, self.zoo.animals)
+
+    def test_calc_daily_incomes(self):
+        lion = Animal('Lion', 2, 'Pesho', 'Female', 25)
+        self.zoo.accommodate(lion)
+        self.assertEqual(self.zoo.calculate_daily_income(), 120)
 
 if __name__ == "__main__":
     unittest.main()
